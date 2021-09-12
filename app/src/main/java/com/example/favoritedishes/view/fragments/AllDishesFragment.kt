81581207bdcbf18527e485c2b8a80fadae2810aa
Vladimir Vorozhtcov <Lilith.2014@mail.ru>
2021-09-12 15:42:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/favoritedishes/view/fragments/AllDishesFragment.kt
@@ -0,0 +1,67 @@
+package com.example.favoritedishes.view.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.favoritedishes.R
+import com.example.favoritedishes.databinding.FragmentAllDishesBinding
+import com.example.favoritedishes.view.activities.AddUpdateDishActivity
+import com.example.favoritedishes.viewmodel.HomeViewModel
+
+class AllDishesFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentAllDishesBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentAllDishesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_dishes, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_add_dish -> {
+                startActivity(Intent(requireActivity(), AddUpdateDishActivity::class.java))
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
