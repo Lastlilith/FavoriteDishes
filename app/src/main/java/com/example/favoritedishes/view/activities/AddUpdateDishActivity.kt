3f6778a240a785dc2626a082b31c68b8b0865341
Vladimir Vorozhtcov <Lilith.2014@mail.ru>
2021-09-20 18:17:01 +0900

    Second commit

--- app/src/main/java/com/example/favoritedishes/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/favoritedishes/view/activities/AddUpdateDishActivity.kt
@@ -1,12 +1,60 @@
 package com.example.favoritedishes.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.favoritedishes.R
+import com.example.favoritedishes.databinding.ActivityAddUpdateDishBinding
+import com.example.favoritedishes.databinding.DialogCustomImageSelectionBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
+
+        mBinding.ivAddDishImage.setOnClickListener(this)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener{
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.iv_add_dish_image -> {
+                customImageSelectionDialog()
+                return
+            }
+        }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener{
+            Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        binding.tvGallery.setOnClickListener{
+            Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
